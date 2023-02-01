7a075e7b306faaa866ca76daf60635f739c3b1fd
Ariel Ohayon <83784945+Ariel-Ohayon@users.noreply.github.com>
2023-02-01 18:34:34 +0200

    Update Memory.v

--- verilog/hdl/Memory.v
+++ verilog/hdl/Memory.v
@@ -7,7 +7,15 @@ module Memory (
 	output	[15:0]	data_out);
 
 	reg	[15:0]	mem [0:11];
-
+	
+	initial
+	begin
+		mem[0] = 16'h3002;
+		mem[1] = 16'h1003;
+		mem[2] = 16'h0007;
+		mem[3] = 16'h0005;
+	end
+	
 	assign data_out = Read ? mem[address] : 16'bz;
 
 	always @(posedge clk)
