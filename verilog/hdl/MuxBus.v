344d86b448586ad58c5083909e9307dfbdf261b0
Ariel Ohayon <83784945+Ariel-Ohayon@users.noreply.github.com>
2023-02-03 08:25:06 +0200

    Update MuxBus.v

--- verilog/hdl/MuxBus.v
+++ verilog/hdl/MuxBus.v
@@ -1,21 +1,21 @@
-module MuxBus(
-	input		[2:0]	selector,
+module MuxBus #(parameter Bits = 16) (
+	input		[2:0]		selector,
 
-	input		[11:0]	ar,
-	input		[11:0]	pc,
+	input		[11:0]		ar,
+	input		[11:0]		pc,
 
-	input		[15:0]	dr,
-	input		[15:0]	ac,
-	input		[15:0]	ir,
-	input		[15:0]	tr,
-	input		[15:0]	mem,
+	input		[Bits-1:0]	dr,
+	input		[Bits-1:0]	ac,
+	input		[Bits-1:0]	ir,
+	input		[Bits-1:0]	tr,
+	input		[Bits-1:0]	mem,
 
-	output reg	[15:0]	Bus);
+	output reg	[Bits-1:0]	Bus);
 
 	always @(selector, ar,pc,dr,ac,ir,tr,mem)
 	begin
 		case(selector)
-			3'd0: Bus = 16'd0;
+			3'd0:   Bus = 0;
 			3'd1:	Bus = ar;
 			3'd2:	Bus = pc;
 			3'd3:	Bus = dr;
