97b8387148f350ca3e8b48fcb3895062e6837f76
Ariel Ohayon <83784945+Ariel-Ohayon@users.noreply.github.com>
2023-02-03 08:24:40 +0200

    Update Memory.v

--- verilog/hdl/Memory.v
+++ verilog/hdl/Memory.v
@@ -1,19 +1,23 @@
-module Memory (
-	input		clk,
-	input		Write,
-	input		Read,
-	input	[11:0]	address,
-	input	[15:0]	data_in,
-	output	[15:0]	data_out);
+module Memory #(parameter Bits = 16) (
+	input				clk,
+	input				Write,
+	input				Read,
+	input	[11:0]		address,
+	input	[Bits-1:0]	data_in,
+	output	[Bits-1:0]	data_out);
 
-	reg	[15:0]	mem [0:11];
+	reg	[Bits-1:0]	mem [0:256];
 	
 	initial
 	begin
-		mem[0] = 16'h2002;
-		mem[1] = 16'h1003;
-		mem[2] = 16'h0007;
-		mem[3] = 16'h0005;
+		mem[0]	= 16'h4005;
+		mem[5]	= 16'h200B;
+		mem[6]	= 16'h7200;
+		mem[7]	= 16'h7020;
+		mem[8]	= 16'h100A;
+		mem[9]	= 16'h3064;
+		mem[10]	= 16'h003A;
+		mem[11]	= 16'h0022;
 	end
 	
 	assign data_out = Read ? mem[address] : 16'bz;
