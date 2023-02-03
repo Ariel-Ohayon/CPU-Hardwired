899e57b6b4d4d70d598ed4bde8621f8eb4e2d5e6
Ariel Ohayon <83784945+Ariel-Ohayon@users.noreply.github.com>
2023-02-03 08:22:33 +0200

    Update Add_and_Logic.v

--- verilog/hdl/Add_and_Logic.v
+++ verilog/hdl/Add_and_Logic.v
@@ -1,33 +1,33 @@
-module Add_and_Logic (
-	input		[15:0]	AC,
-	input		[15:0]	DR,
-	input		[15:0]	inpr,
-	input	 	 [6:0]	opcode,
-	output reg	[16:0]	out);
+module Add_and_Logic #(parameter Bits = 16) (
+	input		[Bits-1:0]	AC,
+	input		[Bits-1:0]	DR,
+	input		[Bits-1:0]	inpr,
+	input	 	[6:0]		opcode,
+	output reg	[Bits:0]	out);
 	
 	always @(AC,DR,opcode)
 	begin
 		if (opcode[0] == 1'b1)	//AND
 		begin
 			out = AC ^ DR;
-			out[16] = 1'b0;
+			out[Bits] = 1'b0;
 		end
 		if (opcode[1] == 1'b1)	// ADD
 			out = AC + DR;
 		if (opcode[2] == 1'b1)	// DR
 		begin
 			out = DR;
-			out[16] = 1'b0;
+			out[Bits] = 1'b0;
 		end
 		if (opcode[3] == 1'b1)	// INPR
 		begin
 			out = inpr;
-			out[16] = 1'b0;
+			out[Bits] = 1'b0;
 		end
 		if (opcode[4] == 1'b1)	// COM
 		begin
 			out = ~AC;
-			out[16] = 1'b0;
+			out[Bits] = 1'b0;
 		end
 		if (opcode[5] == 1'b1)	// SHR
 			out = {AC[0],out[16],AC[15:1]};
